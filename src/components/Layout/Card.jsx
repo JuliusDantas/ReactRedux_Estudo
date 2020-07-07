c3a127282b73c9eccb7cf6007311cc315411647d
Julius Cesar Machado Dantas <juliusmdantas@hotmail.com>
2020-07-07 10:39:46 -0300

    material UI

--- src/components/Layout/Card.jsx
+++ src/components/Layout/Card.jsx
@@ -1,6 +1,9 @@
 import './Card.css'
 import React from 'react'
 
+import Button from '@material-ui/core/Button';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import SaveIcon from '@material-ui/core/IconButton/IconButton'
 
 export default props => {
 
@@ -9,13 +12,31 @@ export default props => {
         borderColor: props.color || '#795548'
     }
 
-    
+    const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    button: {
+      margin: theme.spacing(1),
+    },
+  }),
+);
+
+
+  const classes = useStyles();
+
     return (
         <div className="card" style={estilo}>
             <div className="title">{props.titulo}</div>
             <div className="content">
                 {props.children}
             </div>
+            <Button
+                variant="contained"
+                color="secondary"
+                className={classes.button}
+                startIcon={<SaveIcon />}
+            >
+                Delete
+            </Button>
         </div>
     );
 
